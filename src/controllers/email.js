f4b30f9172f4c950e214992213aee84ffc42801a
Luan Accioly <acciolyluan@gmail.com>
2023-11-11 01:31:30 -0300

    feat: add function to send email on request

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -1,21 +1,24 @@
 import Emails from "../models/Email/index.js";
+import { sendEmail } from "../nodemailer.js";
 import { buildEmail } from "../templates/emailHTML.js";
 
 export const getEmails = async (_, res) => {
   const emails = await Emails.find();
   const responseBody = {
-    data: emails,
+    data: emails[0].emails,
   };
   res.status(200).json(responseBody);
 };
 
 export const sendNews = async (req, res) => {
   const news = req.body;
-  const ip = req.ip;
+  const emailHTML = buildEmail(news);
+  const emails = await Emails.find();
+  const listOfEmails = emails[0].emails;
 
   console.log("Notícia recebida: ", news.title);
-  const emailHTML = buildEmail(news);
 
-  console.log(emailHTML);
+  sendEmail(emailHTML, listOfEmails, news.title);
+
   return res.status(200).json({ HTML: emailHTML });
 };
